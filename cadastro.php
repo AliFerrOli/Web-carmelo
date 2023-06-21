f756e51cf9a86f7972192598260178d65bfcb4da
AliFerrOli <86800535+AliFerrOli@users.noreply.github.com>
2023-06-21 15:38:24 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleLogin.css">
+    <title>Tela de Cdastrar - Dev Envolvente</title>
+</head>
+<body>
+    <form class="login">
+        <h2>Cadastrar</h2>
+        <div class="box-user">
+            <input type="text" name="" required>
+            <label>Nome Completo</label>
+        </div>
+        <div class="box-user-dividido">
+            <div class="box-user direita">
+                <input type="number" name="" required>
+                <label>Cpf</label>
+            </div>
+            <div class="box-user esquerda">
+                <input type="tel" name="" required>
+                <label>Telefone</label>
+            </div>
+        </div>
+        <div class="box-user">
+            <input type="text" name="" required>
+            <label>Endereço</label>
+        </div>
+        <div class="box-user-dividido">
+            <div class="box-user direita">
+                <input type="password" name="" required>
+                <label>Senha</label>
+            </div>
+            <div class="box-user esquerda">
+                <input type="password" name="" required>
+                <label>Confirmar senha</label>
+            </div>
+        </div>
+        <div class="btnCadastrar">
+            <a href="#" class="btn">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Cadastrar
+            </a>
+        </div>
+        
+    </form>
+</body>
+</html>
